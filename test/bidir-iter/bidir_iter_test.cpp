3b236e349fd4e99b7a3bc151f5b05a536d928929
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-02 18:03:51 -0500

    Change include paths

--- test/bidir-iter/bidir_iter_test.cpp
+++ test/bidir-iter/bidir_iter_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include "../../include/c_bidir_iter/c_bidir_iter.h"
+#include "../../include/bidir-iter/bidir_iter.h"
 
 TEST(dummy_suite, dummy_test) {
     EXPECT_TRUE(true);
