47066ec9cb53accdb290fbca7cafdbbb6e259c2e
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-02 22:38:50 -0500

    Add bidir_iter.h

--- include/bidir-iter/bidir_iter.h
+++ include/bidir-iter/bidir_iter.h
@@ -1,9 +1,16 @@
 #ifndef BIDIR_ITER_H
 #define BIDIR_ITER_H
 
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif // #ifdef __cplusplus
+
+struct bidir_iter {
+    void* (*next)(void* struct_p);
+    bool (*has_next)(const void* struct_p);
+};
 
 #ifdef __cplusplus
 }
